731679f4e4979aac12ceb1e8dc9a1aa7e10c500c
Chai-Programming <83523587+CPScript@users.noreply.github.com>
2022-11-14 13:33:40 -0500

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,9 @@
+Question = input("Woud you like to set upi or assistant?")
+if Question == ("yes")
+print ("Loading...")
+from subprocess import call
+call(["python", "python-helper.py"])
+elif Question == ("no")
+print ("OK!")
+from subprocess import call
+call(["python", "main.py"])
