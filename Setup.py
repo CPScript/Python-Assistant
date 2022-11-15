187e104827c10a2b65e1dc0886c866983601da13
Chai-Programming <83523587+CPScript@users.noreply.github.com>
2022-11-15 12:32:23 -0500

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -1,12 +1,14 @@
+from subprocess import call
 print("Would you like to set up your assistant?")
 print ("Yes/No")
 choice = input("")
 
 if choice == "yes":
     print("Set up started")
-   from subprocess import call
-  call(["python", "Setup2.py"])
+    from subprocess import call
+    call(["python", "Setup2.py"])
 
 if choice == "no":
     print("OK...")
     print("Booting Down")
+
